c5f926700ac61fd9d803b96481cde8df9df338ba
shinyatakahashiort-code <shinya.takahashi.ort@gmail.com>
2025-10-29 12:56:33 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,8 +58,8 @@ st.markdown("## 📝 入力データ")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    age = st.number_input("年齢", min_value=0, max_value=100, value=50)
-    k_avg = st.number_input("K（AVG）- 角膜曲率", min_value=40.0, max_value=8.5, value=6.0, step=0.1, format="%.2f")
+    age = st.number_input("年齢", min_value=3, max_value=18, value=9)
+    k_avg = st.number_input("K（AVG）- 角膜曲率", min_value=7.0, max_value=8.7, value=7.4, step=0.1, format="%.2f")
 
 with col2:
     gender = st.selectbox("性別", [0, 1], format_func=lambda x: "男性" if x == 0 else "女性")
